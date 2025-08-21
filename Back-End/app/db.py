9fe163cf681f432c38b273eb6224e2a82625d309
Julio Figueiredo <julioo.1amorim@gmail.com>
2025-08-21 09:55:19 -0300

    feat: add file to connect to test db

---
+++ Back-End/app/db.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.config.settings import DATABASE_URL
+
+# SQLite precisa de check_same_thread=False
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
