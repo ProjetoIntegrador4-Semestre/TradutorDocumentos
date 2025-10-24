7a41036183b76304b4292d87fb1830ee046abee4
sinkevirns <rafaelsinkevicius07@gmail.com>
2025-10-23 21:28:58 -0300

    fix: resolve bug in CurrentUserResolver.java

--- backendJava/src/main/java/com/example/backend/security/CurrentUserResolver.java
+++ backendJava/src/main/java/com/example/backend/security/CurrentUserResolver.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 import java.util.UUID;
 
 @Component
