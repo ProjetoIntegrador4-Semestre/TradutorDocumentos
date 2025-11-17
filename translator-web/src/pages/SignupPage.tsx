313e27985c4ed023ccb05bada1e314eabdb4d43a
BrunoSakamoto <brunosakar2005@gmail.com>
2025-11-17 20:06:16 -0300

    feat: integrate frontend to cloud

--- translator-web/src/pages/SignupPage.tsx
+++ translator-web/src/pages/SignupPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { register, beginGoogleLogin, humanizeSignupError } from "../services/api";
+import { register, humanizeSignupError } from "../services/api";
 import "../styles/auth.css";
 
 export default function SignupPage() {
