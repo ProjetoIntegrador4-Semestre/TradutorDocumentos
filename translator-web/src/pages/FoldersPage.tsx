a93e841d8bbea3e9e7e05fa40a241396fda892a4
BrunoSakamoto <brunosakar2005@gmail.com>
2025-11-27 11:57:53 -0300

    🔍 fix: update aplication.properties

--- translator-web/src/pages/FoldersPage.tsx
+++
@@ -1,3 +0,0 @@
-export default function FoldersPage() {
-  return <div>Folders Page</div>;
-}
--- translator-web/src/pages/LoginPage.tsx
+++ translator-web/src/pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { login, beginGoogleLogin, humanizeAuthError } from "../services/api";
-import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Importando ícones do react-icons
+import { FaEye, FaEyeSlash } from 'react-icons/fa'; 
 import "../styles/auth.css";
 
 export default function LoginPage() {
